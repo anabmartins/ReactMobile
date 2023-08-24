5f446900a4af7b4217de49b28d1df24bb02758bc
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-24 09:28:46 -0300

    style home changes

--- mobile/src/screens/ScreenHome/index.tsx
+++ mobile/src/screens/ScreenHome/index.tsx
@@ -3,7 +3,7 @@ import { View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
-
+import styles from './styles'
 
 export function Home() {
     const navigation = useNavigation();
@@ -22,41 +22,3 @@ export function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      display:'flex',
-      backgroundColor: '#f5f5f5',
-    },
-    title: {
-      color: 'blue',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      fontFamily:'inter',
-    },
-    texty: {
-      fontSize: 15,
-      marginBottom: 10,
-    },
-    cadastro: {
-      fontSize: 15,
-      marginBottom: 10,
-      color: '#FA321A',
-    },
-    button: {
-      backgroundColor: 'blue',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-      fontWeight: 'bold',
-    },
-  })
-  
-  
